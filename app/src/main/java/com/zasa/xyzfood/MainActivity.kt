7991005b1cb7b32a9c3efa7197505c72d92b9eea
zansangeeth <zansangeeth17@gmail.com>
2022-09-09 12:17:51 +0530

    Initial commit

---
+++ app/src/main/java/com/zasa/xyzfood/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.zasa.xyzfood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zasa.xyzfoods.BranchAdapter
+import com.zasa.xyzfoods.BranchResult
+import com.zasa.xyzfoods.RestaurantApi
+import com.zasa.xyzfoods.Utils
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val branches = mutableListOf<BranchResult>()
+        val branchAdapter = BranchAdapter(this, branches)
+
+        val retrofit =
+            Retrofit.Builder().baseUrl(Utils.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val retrofitService = retrofit.create(RestaurantApi::class.java)
+
+        retrofitService.getBranches().enqueue(object : Callback<List<BranchResult>> {
+            override fun onResponse(
+                call: Call<List<BranchResult>>,
+                response: Response<List<BranchResult>>
+            ) {
+                val body = response.body()
+                Log.i(TAG, "responce : $body")
+                rvBraches.apply {
+                    layoutManager = LinearLayoutManager(this@MainActivity)
+                    adapter = branchAdapter
+                    branches.addAll(body!!)
+                    branchAdapter.notifyDataSetChanged()
+                }
+
+                branchAdapter.setOnItemClickListener(object : BranchAdapter.onItemClickListner{
+                    override fun onItemClick(position: Int) {
+                        val branchIntent = Intent(this@MainActivity, BranchActivity::class.java)
+                        branchIntent.putExtra("image", branches[position].image_url)
+                        branchIntent.putExtra("city", branches[position].name)
+                        branchIntent.putExtra("description", branches[position].description)
+                        startActivity(branchIntent)
+                    }
+
+                })
+            }
+
+            override fun onFailure(call: Call<List<BranchResult>>, t: Throwable) {
+                Log.i(TAG, "failure : $t")
+            }
+
+        })
+
+
+    }
+}
