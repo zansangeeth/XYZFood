2664855e8c80000a8be32754c3f6b7c99f8efc25
zansangeeth <zansangeeth17@gmail.com>
2022-09-11 14:00:52 +0530

    first

--- app/src/main/java/com/zasa/xyzfood/utlis/Utils.kt
+++
@@ -1,9 +0,0 @@
-package com.zasa.xyzfood.utlis
-
-/**
- **@Project -> XYZ Foods
- **@Author -> Sangeeth on 9/9/2022
- */
-object Utils {
-    const val BASE_URL = "https://payable-training-evaluation.firebaseio.com/"
-}
